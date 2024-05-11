a235dafc25667ec170dcfe611080d29ddf95b9d3
Sodelayed <diver_space@mail.ru>
2024-05-11 21:05:35 +0300

    финал

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require("doteenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -6,14 +8,14 @@ const routes = require("./routes");
 const port = 4000;
 const app = express();
 
+app.use(express.static("../frontend/build"));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/", routes);
 
-mongoose
-  .connect("mongodb+srv://Sodelayed:dimasik258@teacup.jwwqikp.mongodb.net/")
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server started on port ${port}`);
-    });
+mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
   });
+});
--- backend/helpers/token.js
+++ backend/helpers/token.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const sign = "test";
+const sign = process.env.JWT_SECRET;
 
 module.exports = {
   generate(data) {
